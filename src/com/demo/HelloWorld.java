a983b4c24d35166bbff1a8c0bdd07d835cc81e78
irizwan2014 <39158283+irizwan2014@users.noreply.github.com>
2018-05-11 02:27:47 +0530

    Update HelloWorld.java

--- src/com/demo/HelloWorld.java
+++ src/com/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package com.demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Java");
+		System.out.println("Welcome To Java World");
 	}
 
 }
